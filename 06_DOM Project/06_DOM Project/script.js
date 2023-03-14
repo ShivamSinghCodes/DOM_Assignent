9b7e4ff68a6890d9ca63d99dc1b0a5d6e4a572a5
ShivamSinghCodes <[shivamsinghde@gmail.com]>
2023-03-14 18:41:17 +0530

    Assignment 6 done

---
+++ 06_DOM Project/06_DOM Project/script.js
@@ -0,0 +1,9 @@
+// Changing Logo
+
+let image = document.querySelector('header img ');
+image.src= "./assets/ineuron-logo.png";
+
+// Changing Pricing
+
+let price = document.querySelector('.app_price span');
+price.innerText = "$10";
